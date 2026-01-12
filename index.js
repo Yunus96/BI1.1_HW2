c4fade996030f76ea537aea39a506ee5a7d897ff
Yunus96 <mohammadyunus102@gmail.com>
2026-01-12 10:51:41 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { initializeDatabase } = require("./db/db.connect.js");
+const Hotel = require("./models/hotel.model.js");
+
+const app = express();
+
+app.use(express.json());
+
+initializeDatabase();
+
+//CORS configuration
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.post("/hotels", async (req, res) => {
+  try {
+    const hotel = new Hotel(req.body);
+    const savedHotel = await hotel.save();
+
+    res.status(201).json({
+      message: "Hotel created successfully",
+      data: savedHotel,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+app.get("/hotels", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/hotels/:hotelName", async (req, res) => {
+  try {
+    const hotel = await Hotel.findOne({
+      name: req.params.hotelName,
+    });
+
+    if (!hotel) {
+      return res.status(404).json({ message: "Hotel not found" });
+    }
+
+    res.json(hotel);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
